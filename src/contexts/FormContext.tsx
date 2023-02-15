ed58f223540047763870480a7868f2bd18b78f1d
claytonfortunato <claytonreis10@gmail.com>
2023-02-14 22:31:30 -0300

    feat: pages

---
+++ src/contexts/FormContext.tsx
@@ -0,0 +1,46 @@
+// Context, Reducer, Provider, Hook
+import { createContext, useContext, useReducer } from "react";
+
+const initialData = {
+  currentStep: 0,
+  name: "",
+  level: 0,
+  email: "",
+  github: "",
+};
+
+//Context
+const FormContext = createContext(undefined);
+
+// Reducer
+enum FormActions {
+  setCurrentStep,
+  setName,
+  setLevel,
+  setEmail,
+  setGithub,
+}
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case FormActions.setCurrentStep:
+      return { ...state, currentStep: action.payload };
+    case FormActions.setName:
+      return { ...state, name: action.payload };
+    case FormActions.setLevel:
+      return { ...state, level: action.payload };
+    case FormActions.setEmail:
+      return { ...state, email: action.payload };
+    case FormActions.setGithub:
+      return { ...state, github: action.payload };
+    default:
+      return state;
+  }
+};
+
+//Provider
+const FormProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(formReducer, initialData);
+
+  return <FormContext.Provider>{children}</FormContext.Provider>;
+};
---
+++ src/pages/FormStep3/index.tsx
@@ -0,0 +1,5 @@
+import * as C from "./styles";
+
+export const FormStep3 = () => {
+  return <C.Container>Passo3</C.Container>;
+};
